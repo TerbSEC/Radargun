9db89469a1e1cea15c31edf1f89cf90d84bba23d
AitoW23 <81420883+AitoW23@users.noreply.github.com>
2021-05-31 13:06:15 +0300

    Muutin koodia hiuka

--- config.lua
+++ config.lua
@@ -1,6 +1,6 @@
 cfg = {}
 
-cfg.menuopen = 246 -- BUTTON TO OPEN MENU (https://docs.fivem.net/game-references/controls/)
+cfg.menuopen = 246 -- Painike millä sä avaat sen tutkan
 cfg.bottomfreeze = 38 -- Jäädytä nopeus (https://docs.fivem.net/game-references/controls/)
 cfg.metric = false --// IF METRIC = FALSE THEN IT WILL SHOW AS KM/H, IF METRIC = TRUE, THEN IT WILL CHANGE TO MPH
 cfg.radargun = "WEAPON_FLAREGUN"
