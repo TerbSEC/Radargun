a8c1a8ff5d6092ec24f52b412e86b9cd6771ec6a
AitoW23 <81420883+AitoW23@users.noreply.github.com>
2021-05-31 12:54:24 +0300

    Update config.lua

--- config.lua
+++ config.lua
@@ -11,4 +11,4 @@ cfg = {}
 cfg.menuopen = 246 -- BUTTON TO OPEN MENU (https://docs.fivem.net/game-references/controls/)
 cfg.bottomfreeze = 38 -- FREEZE (https://docs.fivem.net/game-references/controls/)
 cfg.metric = false --// IF METRIC = FALSE THEN IT WILL SHOW AS KM/H, IF METRIC = TRUE, THEN IT WILL CHANGE TO MPH
-cfg.radargun = "WEAPON_VINTAGEPISTOL"
+cfg.radargun = "WEAPON_FLAREGUN"
