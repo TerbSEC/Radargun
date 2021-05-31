c593d7479f087959e76a72f73ea2de8a17405504
AitoW23 <81420883+AitoW23@users.noreply.github.com>
2021-05-31 13:18:47 +0300

    vaihdettu kp/hsta km/hon

--- config.lua
+++ config.lua
@@ -2,5 +2,5 @@ cfg = {}
 
 cfg.menuopen = 246 -- Painike millä sä avaat sen tutkan
 cfg.bottomfreeze = 38 -- Jäädytä nopeus (https://docs.fivem.net/game-references/controls/)
-cfg.metric = false --// IF METRIC = FALSE THEN IT WILL SHOW AS KM/H, IF METRIC = TRUE, THEN IT WILL CHANGE TO MPH
+cfg.metric = true --// IF METRIC = FALSE THEN IT WILL SHOW AS KM/H, IF METRIC = TRUE, THEN IT WILL CHANGE TO MPH
 cfg.radargun = "WEAPON_FLAREGUN"
